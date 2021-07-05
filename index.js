a14e1e36a8aafc5dc3b47dc4c138874c5687a2bf
Luke Teal <l.teal21@gmail.com>
2021-07-05 15:00:29 -0400

    fix view employees. udpate to be async/await for view actions add update employee fuunctionality

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const mysql = require('mysql');
 const inquirer = require('inquirer');
 require('dotenv').config();
+const util = require('util')
 
 const connection = mysql.createConnection({
     host: 'localhost',
@@ -13,11 +14,9 @@ const connection = mysql.createConnection({
     password: process.env.DB_PASSWORD,
     database: 'employee_tracker',
 });
+const query = util.promisify(connection.query.bind(connection))
 
-connection.connect((err) => {
-    if (err) throw err;
-    runEmployeeTracker();
-});
+
 
 function runEmployeeTracker() {
     inquirer.prompt({
@@ -64,29 +63,10 @@ function runEmployeeTracker() {
                 viewEmployees();
                 break;
 
-            case 'View department budget':
-                // viewDeptBudget();
-                break;
 
             case 'Update employee':
-                // updateEmployee();
-                break;
-
-            case 'Update employee manager':
-                // updateManager();
-                break;
-
-            case 'Delete department':
-                // deleteDepartment();
-                break;
-
-            case 'Delete role':
-                // deleteRole();
-                break;
-
-            case 'Delete employee':
-                // deleteEmployee();
-                break;       
+                updateEmployee();
+                break;  
 
             default:
                 console.log(`Invalid action: ${answer.action}`);
@@ -95,7 +75,7 @@ function runEmployeeTracker() {
     });
 }
 
-//  TODO: Add department
+//  Add department
 const addDepartment = () => {
     inquirer.prompt({
         name: 'department',
@@ -116,7 +96,7 @@ const addDepartment = () => {
     })
 }
 
-//  TODO: Add role 
+//  Add role 
 const addRole = () => {
     connection.query(
         'SELECT * FROM departments', (err, res) => {
@@ -167,7 +147,7 @@ const addRole = () => {
         )          
     })
 }
-//  TODO: Add employee
+//  Add employee
 const addEmployee = () => {
     const rolesArray = [];
     const employeesArray = [];
@@ -177,14 +157,14 @@ const addEmployee = () => {
             resRoles.forEach(({Title}) => {
                 rolesArray.push(Title)
             })
-            // console.log(rolesArray)
+
             connection.query(
                 'SELECT * FROM Employees', (err, resEmployees) => {
                     if (err) throw err;
                     resEmployees.forEach(({FirstName, LastName}) => {
                         employeesArray.push(`${FirstName} ${LastName}`)
                     })
-                    // console.log(employeesArray)
+
                     inquirer.prompt([
                         {
                             name: 'firstName',
@@ -243,45 +223,130 @@ const addEmployee = () => {
     )
 }
 
-//  TODO: View department
+//  View department
 
-const viewDepartments = () => {
-    connection.query('SELECT DeptName AS Department FROM Departments', (err, res) => {
-        if(err) throw err;
-        console.table(res)
+const viewDepartments = async () => {
+    response = await query('SELECT DeptName AS Department FROM Departments')
+        console.table(response)
         runEmployeeTracker();
-    })}
+    }
 
-//  TODO: View roles
+//  View roles
 
-const viewRoles = () => {
-    connection.query(`SELECT Title, Salary, DeptName AS Department 
-    FROM Roles LEFT JOIN Departments ON Roles.DepartmentID = Departments.DepartmentID`, (err, res) => {
-        if(err) throw err;
-        console.table(res)
+const viewRoles = async () => {
+    response = await query(`SELECT Title, Salary, DeptName AS Department 
+    FROM Roles LEFT JOIN Departments ON Roles.DepartmentID = Departments.DepartmentID`)
+        console.table(response)
         runEmployeeTracker();
-})}
+}
 
-//  TODO: View employees
-const viewEmployees = () => {
-    connection.query(
+//  View employees
+const viewEmployees = async () => {
+    response = await query(
         `SELECT CONCAT(e.FirstName, ' ', e.LastName) AS Employee, Title AS Role, Salary, DeptName AS Department, CONCAT(m.FirstName, ' ', m.LastName) AS Manager 
-        FROM Employees AS e 
-        JOIN Employees AS m ON m.EmployeeID = e.ManagerID 
-        JOIN Roles ON Roles.RoleID = e.EmployeeID
-        JOIN Departments ON Roles.DepartmentID = Departments.DepartmentID`, 
-        (err, res) => {
-            if(err) throw err;
-            console.table(res)
-            runEmployeeTracker();
-})}
+        FROM Employees e 
+        LEFT JOIN Roles ON Roles.RoleID = e.RoleID
+        LEFT JOIN Departments ON Roles.DepartmentID = Departments.DepartmentID
+		LEFT JOIN Employees AS m ON m.EmployeeID = e.ManagerID `) 
+        console.table(response)
+        runEmployeeTracker();
+}
 
-//  TODO: Update Employee
+//  Update Employee
+const updateEmployee = () => {
+    const rolesArray = [];
+    const employeesArray = [];
+    connection.query(
+        'SELECT * FROM Roles', (err, resRoles) => {
+            if (err) throw err;
+            resRoles.forEach(({Title}) => {
+                rolesArray.push(Title)
+            })
 
+            connection.query(
+                'SELECT * FROM Employees', (err, resEmployees) => {
+                    if (err) throw err;
+                    resEmployees.forEach(({FirstName, LastName}) => {
+                        employeesArray.push(`${FirstName} ${LastName}`)
+                    })
 
-//  TODO: View total department budget
+                    inquirer.prompt([
+                        {
+                            name: 'employee',
+                            type: 'rawlist',
+                            choices: employeesArray,
+                            meassage: 'What employee are you updating?',
+                        },
+                        {
+                            name: 'firstName',
+                            type: 'input',
+                            message: 'Now input the employees updated information. \nEmployees First Name?'
+                        },
+                        {
+                            name: 'lastName',
+                            type: 'input',
+                            message: `Employees Last Name?'`
+                        },
+                        {
+                            name: 'role',
+                            type: 'rawlist',
+                            choices: rolesArray,
+                            message: 'What is this employees role?',
+                        },
+                        {
+                            name: 'manager',
+                            type: 'rawlist',
+                            choices: employeesArray,
+                            meassage: 'Who is this employees manager?',
+                        }
+                
+                    ]).then((answer) => {
+                        let chosenRole;
+                        resRoles.forEach((role)=> {
+                            if(role.Title === answer.role) {
+                                chosenRole = role
+                            }
+                        })
+                        console.log(chosenRole)
+                        let chosenManager;
+                        resEmployees.forEach((manager)=> {
+                            if(`${manager.FirstName} ${manager.LastName}` === answer.manager) {
+                                chosenManager = manager
+                            }
+                        })
+                        let chosenEmployee;
+                        resEmployees.forEach((employee)=> {
+                            if(`${employee.FirstName} ${employee.LastName}` === answer.employee) {
+                                chosenEmployee = employee
+                            }
+                        })
+                        connection.query(
+                            'UPDATE Employees SET ? WHERE ?',
+                            [
+                                {
+                                    FirstName: answer.firstName,
+                                    LastName: answer.lastName,
+                                    RoleID: chosenRole.RoleID,
+                                    ManagerID: chosenManager.EmployeeID
+                                }, 
+                                {
+                                    EmployeeID: chosenEmployee.EmployeeID
+                                },
+                            ],  (err, res) => {
+                                if (err) throw err;
+                                console.log(`Employee updated!\n`);
+                                runEmployeeTracker();
+                            }            
+                        )
+                    }
+                )
+                }
+            )
+        }
+    )
+}
 
-//  TODO: Update Empoyee manager
-//  TODO: Delete department
-//  TODO: Delete role
-//  TODO: Delete employee
+connection.connect((err) => {
+    if (err) throw err;
+    runEmployeeTracker();
+});
